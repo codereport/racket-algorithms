6074446e5f72656991778618b67c15b2088643ab
Conor Hoekstra <codereport@outlook.com>
2020-05-17 00:04:28 -0400

    Bump version to 0.3

--- info.rkt
+++ info.rkt
@@ -4,5 +4,5 @@
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/algorithms.scrbl" ())))
 (define pkg-desc "A package containing many useful algorithms (borrowed from many other programming languages).")
-(define version "0.2")
+(define version "0.3")
 (define pkg-authors '(cph))
