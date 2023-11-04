3e4055a1acce98afc3f8045865920b9e9cf8ee67
Filip Lajszczak <filip@lajszczak.dev>
2023-11-04 11:36:47 +0000

    sets license information in info.rkt to make pkgs.racket-lang.org happy.

--- info.rkt
+++ info.rkt
@@ -2,6 +2,7 @@
 (define collection "algorithms")
 (define deps '("base"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define license 'MIT)
 (define scribblings '(("scribblings/algorithms.scrbl" ())))
 (define pkg-desc "A package containing many useful algorithms (borrowed from many other programming languages).")
 (define version "0.3")
